db3ef008101a81262d1a48f47a4b32acce22b0f2
patoliyap163-web <patoliyap163@gmail.com>
2026-04-28 11:27:28 +0530

    added frontend deployment link to SecurityConfig.java

--- src/main/java/com/example/scholarship_backend/config/SecurityConfig.java
+++ src/main/java/com/example/scholarship_backend/config/SecurityConfig.java
@@ -58,7 +58,8 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:3002",
-                "http://localhost:3001", "http://127.0.0.1:3000", "http://127.0.0.1:3001", "http://127.0.0.1:3002"));
+                "http://localhost:3001", "http://127.0.0.1:3000", "http://127.0.0.1:3001", "http://127.0.0.1:3002",
+                "https://fullstackproject-nu.vercel.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setExposedHeaders(Arrays.asList("Authorization"));
